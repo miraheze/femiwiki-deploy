14e4b5ac5cb7d0f6d346c3b6cf0708fd58e99b42
paladox <thomasmulhall410@yahoo.com>
2023-10-21 13:22:48 +0100

    Update package.json to match REL1_40

--- node_modules/@femiwiki/ooui-femiwiki-theme/build.sh
+++ node_modules/@femiwiki/ooui-femiwiki-theme/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -euo pipefail; IFS=$'\n\t'
+set -euo pipefail
+IFS=$'\n\t'
 
 WD=$(dirname "$0")
-
 
 if [ ! -f "$WD/oojs-ui/yarn.lock" ]; then
   echo 'Installing dependencies'
@@ -13,17 +13,20 @@ echo 'Adding Femiwiki theme'
 yarn grunt --gruntfile "$WD/oojs-ui/Gruntfile.js" add-theme --name=Femiwiki --template=WikimediaUI
 
 echo 'Building Femiwiki theme'
-cat "$WD/src/femiwiki-base.less" >> "$WD/oojs-ui/node_modules/wikimedia-ui-base/wikimedia-ui-base.less"
+cat "$WD/src/femiwiki-base.less" >>"$WD/oojs-ui/node_modules/wikimedia-ui-base/wikimedia-ui-base.less"
 find "$WD/"oojs-ui/src/themes/femiwiki/*.json -exec sed -i 's/"#36c"/"#aca7e2"/g' {} \;
 
 echo 'Building OOUI themes'
 yarn grunt --gruntfile "$WD/oojs-ui/Gruntfile.js" build
 
 echo 'Moveing Femiwiki Theme to dist'
-mkdir -p "$WD/dist/php/" "$WD/dist/resources/"
+mkdir -p "$WD/dist/php/" "$WD/dist/resources/" "$WD/dist/femiwiki/images/images"
 cp "$WD/oojs-ui/php/themes/FemiwikiTheme.php" "$WD/dist/"
 cp "$WD/oojs-ui/dist/oojs-ui-femiwiki.js" "$WD/dist/resources/"
 cp "$WD"/oojs-ui/dist/oojs-ui-femiwiki-*.css "$WD/dist/resources/"
 cp "$WD"/oojs-ui/dist/oojs-ui-*-femiwiki.css "$WD/dist/resources/"
 cp "$WD"/oojs-ui/dist/themes/femiwiki/icons-*.json "$WD/dist/resources/"
 cp -r "$WD/oojs-ui/dist/themes/femiwiki/images" "$WD/dist/resources/images/"
+cp -r "$WD/oojs-ui/dist/themes/femiwiki/images/icons" "$WD/dist/femiwiki/images/icons"
+
+echo "Done"
